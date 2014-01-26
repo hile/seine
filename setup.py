099d5680d2a7666da64e8628a666916ba457af5f
Ilkka Tuohela <hile@iki.fi>
2014-01-26 18:10:30 +0200

    Cleanup

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
-import os,glob
-from setuptools import setup,find_packages
+import os
+import glob
+from setuptools import setup, find_packages
 
 VERSION='2.0.2'
-README = open(os.path.join(os.path.dirname(__file__),'README.txt'),'r').read()
+README = open(os.path.join(os.path.dirname(__file__), 'README.txt'), 'r').read()
 
 setup(
     name = 'seine',
@@ -13,13 +14,18 @@ setup(
     keywords = 'Network Utility Functions',
     url = 'http://tuohela.net/packages/seine',
     zip_safe = False,
-    install_requires = ['systematic','dnspython','pyip','pyasn1','pysnmp'],
     scripts = glob.glob('bin/*'),
     packages = ['seine'] + ['seine.%s'%s for s in find_packages('seine')],
     author = 'Ilkka Tuohela',
     author_email = 'hile@iki.fi',
     description = 'Various network address and url related utilities',
     long_description = README,
-
+    install_requires = [
+        'systematic',
+        'dnspython',
+        'pyip',
+        'pyasn1',
+        'pysnmp'
+    ],
 )
 
