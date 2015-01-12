6219519d7e6b645696acb15f108630667471561b
Ilkka Tuohela <hile@iki.fi>
2015-01-12 07:23:42 +0200

    Another attempt to fix pip installs

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 import glob
 from setuptools import setup, find_packages
 
-VERSION='2.4.6'
+VERSION='2.4.7'
 
 setup(
     name = 'seine',
@@ -12,13 +12,11 @@ setup(
     license = 'PSF',
     keywords = 'Network Utility Functions',
     url = 'http://tuohela.net/packages/seine',
-    zip_safe = False,
-    scripts = glob.glob('bin/*'),
-    packages = ['seine'] + ['seine.%s'%s for s in find_packages('seine')],
     author = 'Ilkka Tuohela',
     author_email = 'hile@iki.fi',
     description = 'Various network address and url related utilities',
-    install_requires = [
+    packages = ['seine'] + ['seine.%s'%s for s in find_packages('seine')],
+    install_requires = (
         'systematic>=4.2.3',
         'requests',
         'python-dateutil',
@@ -26,6 +24,6 @@ setup(
         'pyip',
         'pyasn1',
         'pysnmp'
-    ],
+    ),
 )
 
